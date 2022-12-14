231d7be46b7ec9fb87fb422079d7b546d43ea1a6
Orjihenry <orjihen@yahoo.com>
2022-12-14 11:07:12 +0000

    isalpha 🌹🌹🌹

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,14 @@
 /**
  * _isalpha - Check for alphabetic character.
  *
+ * @c: a character to be checked
+ *
  * Return: 1 or 0
  */
 
 int _isalpha(int c)
 {
-	if (c >= 65 && c <= 90 && c >= 97
+	if (c >= 65 && c <= 90 || c >= 97
 			&& c <= 122)
 	{
 		return (1);
