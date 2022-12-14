0568d13e832c7e4f01c2d79ee658c70ee965e2e3
Orjihenry <orjihen@yahoo.com>
2022-12-14 16:12:03 +0000

    Sign 🔥🔥

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,5 +10,17 @@
 
 int _print_sign(int n)
 {
-	
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		_putchar('0');
+		return (0);
 }
