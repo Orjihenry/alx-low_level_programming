9f3748d6f294d42e985b49449ce72d6502589198
Orjihenry <orjihen@yahoo.com>
2022-12-14 02:16:49 +0000

    islower 🔥🔥🔥🔥🔥

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _islower -  Prints 10 times the alphabet, in lowercase
+ *		followed by a new line.
+ *
+ * Return: 1 or 0
+ */
+
+int int _islower(void)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
