0b10e06db1321f90976f112a7b7b12bce36ab2c5
Orjihenry <orjihen@yahoo.com>
2022-12-15 23:22:54 +0000

    bug fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,28 @@
 
 int main(void)
 {
-	int num;
+	int i;
 
-	for (num = 1; num <= 100; ++num)
+	for (i = 1; i <= 100; i++)
 	{
-		if (num % 3 == 0 && !(num % 5 == 0))
-			printf("Fizz");
-		else if (num % 5 == 0 && !(num % 3 == 0))
-			printf("Buzz");
-		else if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
-		else
-			printf("%d", num);
-
-		if (num != 100)
-			printf(" ");
-		else
-			printf(10);
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
 	}
+	printf(10);
 
 	return (0);
 }
