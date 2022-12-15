b6d852628068cd74ebe3d7c1d0e26d0e5af32e16
Orjihenry <orjihen@yahoo.com>
2022-12-15 00:06:33 +0000

    a + b => 🅰️ ➕🅱️

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - Program that adds two integers and returns the result.
+ *
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Success 0
+ */
+
+int add(int a, int b)
+{
+	int ch;
+
+	ch = a + b;
+
+	return (ch);
+}
