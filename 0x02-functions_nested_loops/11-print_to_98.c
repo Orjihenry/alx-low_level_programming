a2025910b00c21f1a6661988e0cddb23d8e04217
Orjihenry <orjihen@yahoo.com>
2022-12-15 00:47:51 +0000

    98 Battery Street, the OG😎😎😎

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,28 +11,25 @@
 
 void print_to_98(int n)
 {
+	int i, j;
+
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
-	}
-	else
+	} else if (n >= 98)
 	{
-		for (; n >= 98; n--)
+		for (j = n; j >= 98; j--)
 		{
-			printf("%d", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
-		printf("\n");
 	}
 }
