947aff1f39be0c35dd43e8b28482b55547edca1c
Orjihenry <orjihen@yahoo.com>
2022-12-19 22:51:14 +0000

    I can only go one way. I've not got a reverse gea 🔆🔆🔆🔆

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - Function that prints a string in reverse,
+ *		followed by a new line.
+ *
+ * @s: Parameter to be printed.
+ *
+ * Return: Success 0
+ */
+
+void print_rev(char *s)
+{
+	int i = 0, j, n;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
+
+	for (j = n - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar(10);
+}
