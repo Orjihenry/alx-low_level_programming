df59bc226c6e87ac6edde05a6eedeaf9b7b6007e
Orjihenry <orjihen@yahoo.com>
2022-12-22 22:49:56 +0000

    strcmp ✨✨✨✨✨

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Program tha  compares two strings.
+ *
+ * @s1: string1 to be compared
+ * @s2: string2 to be compared
+ *
+ * Return: less than 0 if s1 is less than s2, 0 if they're equal,
+ * more than 0 if s1 is greater than s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
