4118082accbb6e3c5f3881ccabdbf267637d4424
Orjihenry <orjihen@yahoo.com>
2022-12-22 23:03:38 +0000

    Mozart composed his music not for the elite, but for everybody 😎😎😎

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
 
 #endif /* MAIN_H */
