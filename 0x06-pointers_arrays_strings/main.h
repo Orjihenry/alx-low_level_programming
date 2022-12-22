ccbffd49bd23b8009c44ed57c775ced177461f3c
Orjihenry <orjihen@yahoo.com>
2022-12-22 22:58:32 +0000

    Expect the best. Prepare for the worst. Capitalize on what comes ☀️ ☀️ ☀️ ☀️ ☀️ ☀️

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
 
 #endif /* MAIN_H */
