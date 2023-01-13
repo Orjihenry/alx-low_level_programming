89896f0b9986f9f302ef5bb066043feb3242e763
Orjihenry <orjihen@yahoo.com>
2023-01-13 05:05:51 +0000

    Trust no one 💫💫💫💫

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,12 +11,10 @@
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *new_mem;
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	new_mem = malloc(b);
+	if (new_mem == NULL)
 		exit(98);
-
-	return (ptr);
+	return (new_mem);
 }
