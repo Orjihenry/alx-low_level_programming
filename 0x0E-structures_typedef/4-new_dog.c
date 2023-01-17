ed59b35e818395cbbf06f0473088d1e6332d9acc
Orjihenry <orjihen@yahoo.com>
2023-01-17 13:47:59 +0000

    A door is what a dog is perpetually on the wrong side of🐶🐶👌

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,43 @@
 #include "dog.h"
 
 /**
+ * _strlen - Function that gets length of string.
+ *
+ * @str: String to get the length
+ *
+ * Return: length of @str
+*/
+
+int _strlen(const char *str)
+{
+	int length = 0;
+
+	while (*str++)
+		length++;
+	return (length);
+}
+
+/**
+ * _strcopy - Function that returns @dest with a copy of a string from @src.
+ *
+ * @src: string to copy
+ * @dest: copy string to here
+ *
+ * Return: @dest
+*/
+
+char *_strcopy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a]; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+
+	return (dest);
+}
+
+/**
  * new_dog - Function that creates a new dog.
  *
  * @name: name of dog
