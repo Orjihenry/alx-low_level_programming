40139f0dced152939db86e05fba17803531bfe55
Orjihenry <orjihen@yahoo.com>
2023-01-17 15:10:52 +0000

    File name 📂📛📂📛

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Program that prints the name of the file compiled from.
+ *
+ * Return: Success 0
+ */
+
+int main(void)
+{
+	_putchar("%s\n", __FILE__);
+	return (0);
+}
